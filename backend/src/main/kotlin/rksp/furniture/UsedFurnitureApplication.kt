99f156c0db34b674370a683758694c9519f45524
Anton <stepanton94@mail.ru>
2025-05-16 22:16:36 +0300

    add Kotlin main function to launch Spring Boot application without Java entrypoint

--- backend/src/main/kotlin/rksp/furniture/UsedFurnitureApplication.kt
+++ backend/src/main/kotlin/rksp/furniture/UsedFurnitureApplication.kt
@@ -1,5 +1,6 @@
 ﻿package rksp.furniture
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,6 @@ import org.springframework.boot.runApplication
 open class UsedFurnitureApplication
 
 fun main(args: Array<String>) {
-    runApplication<UsedFurnitureApplication>(*args)
+    SpringApplication.run(UsedFurnitureApplication::class.java, *args)
 }
+
